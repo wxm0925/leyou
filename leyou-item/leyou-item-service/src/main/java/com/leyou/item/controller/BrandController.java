58966429ed4d9e5764ae9307a8db2a93d70a2f91
wenxiangmin <673085055@qq.com>
2019-11-05 13:12:54 +0800

    跨域实现+品牌查询

---
+++ leyou-item/leyou-item-service/src/main/java/com/leyou/item/controller/BrandController.java
@@ -0,0 +1,48 @@
+package com.leyou.item.controller;
+
+import com.leyou.common.util.PageResult;
+import com.leyou.item.pojo.Brand;
+import com.leyou.item.service.BrandService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.CollectionUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author ：wen_xm
+ * @date ：Created in 2019/11/5 13:06
+ * @description： 品牌管理
+ */
+@RestController
+@RequestMapping("brand")
+public class BrandController {
+    @Autowired
+    private BrandService brandService;
+
+    /**
+     * 根据查询条件分页并排序查询品牌信息
+     * @param key 模糊搜索关键字
+     * @param page 当前页数
+     * @param rows 页数大小
+     * @param sortBy 根据哪个字段排序
+     * @param desc 是否降序
+     * @return
+     */
+    @GetMapping("page")
+    public ResponseEntity<PageResult<Brand>> queryBrandsByPage(
+            @RequestParam(value = "key", required = false)String key,
+            @RequestParam(value = "page", defaultValue = "1")Integer page,
+            @RequestParam(value = "rows", defaultValue = "5")Integer rows,
+            @RequestParam(value = "sortBy", required = false)String sortBy,
+            @RequestParam(value = "desc", required = false)Boolean desc
+    ){
+        PageResult<Brand> result = this.brandService.queryBrandsByPage(key, page, rows, sortBy, desc);
+        if (CollectionUtils.isEmpty(result.getItems())){
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(result);
+    }
+}
---
+++ leyou-item/leyou-item-service/src/main/java/com/leyou/item/service/BrandService.java
@@ -0,0 +1,56 @@
+package com.leyou.item.service;
+
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import com.leyou.common.util.PageResult;
+import com.leyou.item.mapper.BrandMapper;
+import com.leyou.item.pojo.Brand;
+import org.apache.commons.lang.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import tk.mybatis.mapper.entity.Example;
+
+import java.util.List;
+
+/**
+ * @author ：wen_xm
+ * @date ：Created in 2019/11/5 13:01
+ * @description：
+ */
+@Service
+public class BrandService {
+    @Autowired
+    private BrandMapper brandMapper;
+
+    /**
+     *@描述 根据分页调节查询结果集
+     *@参数
+     *@返回值
+     *@创建人 wen_xm
+     *@创建时间 13:01
+     */
+    public PageResult<Brand> queryBrandsByPage(String key, Integer page, Integer rows, String sortBy, Boolean desc){
+        //初始化example对象
+        Example example = new Example(Brand.class);
+        Example.Criteria criteria = example.createCriteria();
+
+        // 根据name模糊查询，或者根据首字母查询
+        if (StringUtils.isNotBlank(key)) {
+            criteria.andLike("name", "%" + key + "%").orEqualTo("letter", key);
+        }
+
+        // 添加分页条件  page:当前页 ， rows：每页大小
+        PageHelper.startPage(page, rows);
+
+        // 添加排序条件
+        if(StringUtils.isNotBlank(sortBy)) {
+            example.setOrderByClause(sortBy + " " + (desc ? "desc" : "asc"));
+        }
+        List<Brand> brands = this.brandMapper.selectByExample(example);
+        // 包装成pageInfo
+        PageInfo<Brand> pageInfo = new PageInfo<>(brands);
+        // 包装成分页结果集返回
+        return new PageResult<>(pageInfo.getTotal(), pageInfo.getList());
+    }
+
+}
